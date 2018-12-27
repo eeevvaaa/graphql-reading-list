a6499b4a92c179b96065261e8d2edfdad7b22427
Eva <evalee904@gmail.com>
2018-12-27 12:04:49 -0600

    Set up root query and import schema

--- server/index.js
+++ server/index.js
@@ -1,11 +1,18 @@
 const express = require('express');
 // convention for GraphQL where the variable does not match the name of the package.
 const graphqlHTTP = require('express-graphql');
+const schema = require('./schema/schema');
 
 const app = express();
 
 // middleware
-app.use('/graphql', graphqlHTTP({}));
+app.use(
+	'/graphql',
+	graphqlHTTP({
+		// schema: schema
+		schema
+	})
+);
 
 app.listen(4000, () => {
 	console.log('\n--- Server running on localhost:4000 ---');
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,7 +1,7 @@
 const graphql = require('graphql');
 
 // define object type. pay attention to the capitalization
-const { GraphQLObjectType, GraphQLString } = graphql;
+const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
 
 // define a schema, how a graph will look
 const BookType = new GraphQLObjectType({
@@ -12,4 +12,24 @@ const BookType = new GraphQLObjectType({
 		name: { type: GraphQLString },
 		genre: { type: GraphQLString }
 	})
+});
+
+// root query, the entry point
+const RootQuery = new GraphQLObjectType({
+	name: 'RootQueryType',
+	fields: {
+		book: {
+			type: BookType,
+			// decide which specific book to land on, book(id: '123')
+			args: { id: { type: GraphQLString } },
+			//code to get data from db or other source
+			resolve(parent, args) {
+				args.id;
+			}
+		}
+	}
+});
+
+module.exports = new GraphQLSchema({
+	query: RootQuery
 });

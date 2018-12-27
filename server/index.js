362336557da802252634b4b17b9ba9df35fd1c51
Eva <evalee904@gmail.com>
2018-12-27 17:41:02 -0600

    Added mongoose dependency and set up connection to database

--- server/index.js
+++ server/index.js
@@ -2,8 +2,15 @@ const express = require('express');
 // convention for GraphQL where the variable does not match the name of the package.
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
 
 const app = express();
+
+// connet to mlab database
+mongoose.connect('mongodb://user:test123@ds111963.mlab.com:11963/gql-rdg-ls');
+mongoose.connection.once('open', () => {
+	console.log('\n--- Connected to database ---');
+});
 
 // middleware
 app.use(

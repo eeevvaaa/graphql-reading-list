b14765c6d8ce189dff7633b0edb8677e621e8274
Eva <evalee904@gmail.com>
2018-12-29 23:14:10 -0600

    Added state to AddBook component and handleInput method

--- client/src/components/AddBook.js
+++ client/src/components/AddBook.js
@@ -3,6 +3,19 @@ import { graphql } from 'react-apollo';
 import { getAuthorsQuery } from '../queries/queries';
 
 class AddBook extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			name: '',
+			genre: '',
+			authorId: ''
+		};
+	}
+
+	handleInput = event => {
+		this.setState({ [event.target.name]: event.target.value });
+	};
+
 	displayAuthors() {
 		let data = this.props.data;
 		if (data.loading) {
@@ -22,15 +35,15 @@ class AddBook extends Component {
 			<form className="add-book">
 				<div className="field">
 					<label>Book name:</label>
-					<input type="text" />
+					<input type="text" name="name" onChange={this.handleInput} />
 				</div>
 				<div className="field">
 					<label>Genre:</label>
-					<input type="text" />
+					<input type="text" name="genre" onChange={this.handleInput} />
 				</div>
 				<div className="field">
 					<label>Author:</label>
-					<select>
+					<select name="authorId" onChange={this.handleInput}>
 						<option>Select author</option>
 						{this.displayAuthors()}
 					</select>
